e0c8a382498a2129cda9e69d919ce39d0dff8413
juan <juansoussev@gmail.com>
2023-08-20 17:44:21 -0300

    Logo adicionada à header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Image, Text, View } from 'react-native'
 
 export default class Header extends Component {
   render() {
     return (
-      <Text>Aqui é a Header</Text>
+      <View style={{
+        height: 70,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <Image style={{
+          maxHeight: 60,
+          maxWidth: 60,
+        }} source={require('../../assets/scea-logo.png')} />
+      </View>
     )
   }
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,23 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from '../components/Header'
 
 export default class Home extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>SOEPA App</Text>
-        <Text>Sistema de Orientação nao sei oq lá para pais de autistas</Text>
-        <Text
-          onPress={() => {
-            this.props.navigation.navigate('Professional', {
-              title: 'asdasdasdasdasdasd'
-            })
-          }}
-        >
-          Clique aqui para ir para a tela do profissional
-        </Text>
-      </View>
+      <>
+        <Header />
+        <View style={styles.container}>
+          <Text>SOEPA App</Text>
+          <Text>Sistema de Orientação nao sei oq lá para pais de autistas</Text>
+          <Text
+            onPress={() => {
+              this.props.navigation.navigate('Professional', {
+                title: 'asdasdasdasdasdasd'
+              })
+            }}
+          >
+            Clique aqui para ir para a tela do profissional
+          </Text>
+        </View>
+      </>
     );
   }
 }
